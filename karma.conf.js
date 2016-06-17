595b79296d6e1b1343c14048bdd10a7982d8c8e8
emst <emil.stridfeldt@gmail.com>
2016-06-17 20:38:06 +0200

    Added enzyme test framework.

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = function (config) {
@@ -23,8 +24,14 @@ module.exports = function (config) {
             devtool: 'inline-source-map',
             module: {
                 loaders: [
-                    { test: /\.js$/, loader: 'babel-loader' }
+                    { test: /\.js$/, loader: 'babel-loader', exclude: path.resolve(__dirname, 'node_modules') },
+                    { test: /\.json$/, loader: 'json' }
                 ]
+            },
+            externals: {
+                'react/addons': true,
+                'react/lib/ExecutionEnvironment': true,
+                'react/lib/ReactContext': true
             }
         },
 
--- test/sample-component-test.js
+++ test/sample-component-test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
+import { shallow, mount, render } from 'enzyme';
 import SampleComponent from '../src/sample-component';
 
-describe('root', function () {
-    it('renders without problems', function () {
+describe('sample component', function () {
+    it('renders without problems', () => {
         var sampleComponent = TestUtils.renderIntoDocument(<SampleComponent/>);
         expect(sampleComponent).toExist();
+    });
+
+    it('shallow renders with enzyme without problems', () => {
+        const wrapper = shallow(<SampleComponent/>);
+        expect(wrapper.find('div').length).toBe(1);
+    });
+
+    it('full renders with enzyme without problems', () => {
+        const wrapper = mount(<SampleComponent/>);
+        expect(wrapper.find('div').length).toBe(1);
     });
 });
